408da738a74a030c16d6450015826cbab5e3fe67
Marcus Mesquita <mesquita.marcusv@gmail.com>
2013-06-20 12:28:46 -0300

    Tela CadastroCliente Criada

---
+++ src/frame/CadastroCliente.java
@@ -0,0 +1,145 @@
+package frame;
+
+import java.text.ParseException;
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class CadastroCliente extends JFrame {
+	
+	private JTextField txtNomeCliente;
+	private JTextField txtEndereco;
+	private JFormattedTextField txtTelefone;
+	private JFormattedTextField txtCelular;
+	private final ButtonGroup groupSexo = new ButtonGroup();
+
+	public CadastroCliente(){
+		//Define tamanho e posição da tela
+		setBounds(100, 100, 440, 240);
+		setLocationRelativeTo(null);
+		
+		setTitle("Cadastro de Cliente");
+		
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		getContentPane().setLayout(null);
+		
+		//Labels
+		JLabel lblNomeCliente = new JLabel("Nome do Cliente:");
+		lblNomeCliente.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNomeCliente.setBounds(10, 11, 107, 14);
+		getContentPane().add(lblNomeCliente);
+		
+		JLabel lblTelefoneFixo = new JLabel("Telefone Fixo:");
+		lblTelefoneFixo.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblTelefoneFixo.setBounds(10, 61, 107, 14);
+		getContentPane().add(lblTelefoneFixo);
+		
+		JLabel lblCelular = new JLabel("Celular:");
+		lblCelular.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblCelular.setBounds(10, 86, 107, 14);
+		getContentPane().add(lblCelular);
+		
+		JLabel lblEndereco = new JLabel("Endere\u00E7o:");
+		lblEndereco.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblEndereco.setBounds(10, 36, 107, 14);
+		getContentPane().add(lblEndereco);
+		
+		//Campo nome do Cliente
+		txtNomeCliente = new JTextField();
+		txtNomeCliente.setBounds(127, 8, 295, 20);
+		getContentPane().add(txtNomeCliente);
+		txtNomeCliente.setColumns(10);
+		
+		//Campo endereço
+		txtEndereco = new JTextField();
+		txtEndereco.setBounds(127, 33, 295, 20);
+		getContentPane().add(txtEndereco);
+		txtEndereco.setColumns(10);
+		
+		//Campo telefone
+		txtTelefone = new JFormattedTextField();
+		txtTelefone.setBounds(127, 58, 295, 20);
+		getContentPane().add(txtTelefone);
+		txtTelefone.setColumns(10);
+		//Formato do campo telefone
+		try {
+			txtTelefone.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("####-####")));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		//Campo celular
+		txtCelular = new JFormattedTextField();
+		txtCelular.setBounds(127, 83, 295, 20);
+		getContentPane().add(txtCelular);
+		txtCelular.setColumns(10);
+		//Formato do campo celular
+		try{
+			txtCelular.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("#####-####")));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		//Panel com opções de sexo
+		JPanel panelSexo = new JPanel();
+		panelSexo.setBorder(new TitledBorder(null, "Sexo", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelSexo.setBounds(10, 111, 87, 101);
+		getContentPane().add(panelSexo);
+		panelSexo.setLayout(null);
+		
+		//RadioButton Masculino
+		JRadioButton rdbtnMasculino = new JRadioButton("Masculino");
+		groupSexo.add(rdbtnMasculino);
+		rdbtnMasculino.setBounds(6, 27, 75, 23);
+		panelSexo.add(rdbtnMasculino);
+		
+		//RadioButton Feminino
+		JRadioButton rdbtnFeminino = new JRadioButton("Feminino");
+		groupSexo.add(rdbtnFeminino);
+		rdbtnFeminino.setBounds(6, 53, 75, 23);
+		panelSexo.add(rdbtnFeminino);
+		
+		//Panel de opções
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBounds(120, 135, 304, 52);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		//Botão cadastrar
+		JButton btnCadastrar = new JButton("Cadastrar");
+		btnCadastrar.setBounds(10, 11, 88, 23);
+		panel.add(btnCadastrar);
+		
+		//Botão Limpar
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			
+			}
+		});
+		btnLimpar.setBounds(108, 11, 88, 23);
+		panel.add(btnLimpar);
+		
+		//Botão Voltar
+		JButton btnVoltar = new JButton("Voltar");
+		btnVoltar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				setVisible(false);
+				dispose();
+			}
+		});
+		btnVoltar.setBounds(206, 11, 88, 23);
+		panel.add(btnVoltar);
+		
+	}
+}
--- src/frame/Login.java
+++ src/frame/Login.java
@@ -7,6 +7,10 @@ import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.Toolkit;
 import javax.swing.border.TitledBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Login extends JFrame {
 	
@@ -24,40 +28,77 @@ public class Login extends JFrame {
 		getContentPane().setLayout(null);
 		
 		//Definição do tamanho da tela e posicionamento no centro
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 320);
 		setLocationRelativeTo(null);
 		
 		//Titulo
 		JLabel lblTitle = new JLabel("Ana Mesquita Cabeleireiros");
+		lblTitle.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblTitle.setFont(new Font("Segoe Script", Font.BOLD, 26));
 		lblTitle.setBounds(10, 11, 414, 40);
 		getContentPane().add(lblTitle);
 		
 		//Panel principal
-		JPanel panelOpcoes = new JPanel();
-		panelOpcoes.setBorder(new TitledBorder(null, "Op\u00E7\u00F5es", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panelOpcoes.setBounds(260, 75, 164, 175);
-		getContentPane().add(panelOpcoes);
-		panelOpcoes.setLayout(null);
+		JPanel panelGerenciamento = new JPanel();
+		panelGerenciamento.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Gerenciamento", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelGerenciamento.setBounds(10, 62, 164, 124);
+		getContentPane().add(panelGerenciamento);
+		panelGerenciamento.setLayout(null);
 		
 		//Botão Atendimento
 		JButton btnAtendimento = new JButton("Atendimento");
-		btnAtendimento.setBounds(10, 23, 144, 23);
-		panelOpcoes.add(btnAtendimento);
+		btnAtendimento.setBounds(10, 22, 144, 23);
+		panelGerenciamento.add(btnAtendimento);
 		
 		//Botao Agenda
 		JButton btnAgenda = new JButton("Agenda");
-		btnAgenda.setBounds(10, 57, 144, 23);
-		panelOpcoes.add(btnAgenda);
+		btnAgenda.setBounds(10, 56, 144, 23);
+		panelGerenciamento.add(btnAgenda);
 		
 		//Botao Consulta
 		JButton btnConsulta = new JButton("Consulta");
-		btnConsulta.setBounds(10, 94, 144, 23);
-		panelOpcoes.add(btnConsulta);
+		btnConsulta.setBounds(10, 90, 144, 23);
+		panelGerenciamento.add(btnConsulta);
+		
+		//Painel Cadastro
+		JPanel panelCadastro = new JPanel();
+		panelCadastro.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Cadastro", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelCadastro.setBounds(10, 187, 164, 95);
+		getContentPane().add(panelCadastro);
+		panelCadastro.setLayout(null);
+		
+		//Botão Cadastrar Serviço
+		JButton btnCadastrarServio = new JButton("Cadastrar Servi\u00E7o");
+		btnCadastrarServio.setBounds(10, 61, 144, 23);
+		panelCadastro.add(btnCadastrarServio);
+		
+		//Botão Cadastrar Clientes
+		JButton btnCadastrarCliente = new JButton("Cadastrar Cliente");
+		btnCadastrarCliente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				CadastroCliente novaTelaCadastroCliente = new CadastroCliente();
+				novaTelaCadastroCliente.setVisible(true);
+			}
+		});
+		btnCadastrarCliente.setBounds(10, 27, 144, 23);
+		panelCadastro.add(btnCadastrarCliente);
+		
+		//Painel do botão Sair
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBounds(268, 234, 164, 48);
+		getContentPane().add(panel);
+		panel.setLayout(null);
 		
 		//Botao Sair
-		JButton btnSair = new JButton("Sair");
-		btnSair.setBounds(10, 141, 144, 23);
-		panelOpcoes.add(btnSair);
+		JButton btnSair_1 = new JButton("Sair");
+		btnSair_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				setVisible(false);
+				dispose();
+			}
+		});
+		btnSair_1.setBounds(10, 11, 144, 23);
+		panel.add(btnSair_1);
 	}
 }
